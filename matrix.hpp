4e8fd019ac29d9887976f419e8a01770572dbafb
Mateusz Mazur <mateusz.mazur@e.email>
2023-07-03 08:41:09 +0200

    Add OpenMP SIMD

--- matrix.hpp
+++ matrix.hpp
@@ -67,6 +67,7 @@ template <size_t Size> class PolyMatrix {
         assert(this != &c && &b != &c);
 
         for (int i = 0; i < 4; i++) {
+            #pragma omp simd collapse(2)
             for (int j = 0; j < 4; j++) {
                 Polynomial res = 0;
                 for (int k = 0; k < 4; k++) {
